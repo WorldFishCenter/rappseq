e4cd434f87a7711f4ec9d7d8c3e489fb85413908
Fernando Cagua <efcaguab@gmail.com>
2021-11-04 14:59:15 +1300

    Redo the identify page to include classifier

--- site/content/identify/_index.md
+++ site/content/identify/_index.md
@@ -1,6 +1,7 @@
 ---
 title: Identification tool
 assets_js:
+    - js/hs-file-attach.min.js
     - js/json2html.js
     - js/identify.js
 ---
--- site/themes/rappseq/assets/js/identify.js
+++ site/themes/rappseq/assets/js/identify.js
@@ -1,133 +1,69 @@
+$(document).on('ready', function () {
+  // INITIALIZATION OF CUSTOM FILE
+  // =======================================================
+  $('.js-file-attach').each(function () {
+    var customFile = new HSFileAttach($(this)).init();
+  });
+});
 
 
 var results = document.querySelector('.results');
-let spinner = document.querySelector('#submit-spinner')
-let button = document.querySelector('#submit-button')
+let spinner = document.querySelector('#submit-spinner');
+let button = document.querySelector('#submit-button');
+let email = document.querySelector('input[type="email"]');
+let fileField = document.querySelector('input[type="file"]');
 
-let row_template = {
-    '<>': "tr", 
-    'html': [
-        {'<>': "th", 'text': "${name}", "scope": "row"},
-        {'<>': "td", 'text': "${value}"}
-    ]
+let valid_file = false;
+let valid_classifier = false;
+let valid_form = false;
+
+button.disabled = true;
+
+function validate_file() {
+    if (fileField.files.length > 0) {
+        valid_file = true;
+        //fileField.classList.remove("is-invalid");
+        //fileField.classList.add("is-valid");
+    } else {
+        valid_file = false;
+        //fileField.classList.remove("is-valid");
+        //fileField.classList.add("is-invalid");
+    }
 }
 
-json2html.component.add('row',row_template);
-
-let table_template = {
-    '<>': "table",
-    'class': "table", 
-    'html': [
-        {'<>': "thead", 'class': "", 'html': [
-            {'<>': "tr", 'html': [
-                {'<>': "th", "text": "${type}", "scope": "col"}, 
-                {'<>': "th", "text": "Number of matches", "scope": "col"}
-            ]}
-        ]},
-        {'<>': "tbody", 'html': [{'[]': 'row', "obj": function(){return(this.matches);}}]}
-    ]
+function validate_classifier() {
+    let checked_checboxes = document.querySelectorAll('input[name="classifier"]:checked');
+    if (checked_checboxes.length > 0) {
+        valid_classifier = true;
+    } else {  
+        valid_classifier = false;
+    }
 }
 
-json2html.component.add('table',table_template);
+function validate_form(){
 
-let hidden_table_template = {
-    '<>': "div", 'id': "${id}-accordion", 'html': [
-        {'<>': "div", 'class': "card overflow-hidden mb-3", 'html': [
-            {'<>': "div", 'class': "card-header card-collapse", 'id': '${id}-table-heading', 'html': [
-                {'<>': "button", 'class': "btn btn-link btn-block card-btn d-flex justify-content-between p-3 text-primary collapsed", 'data-toggle': 'collapse', 'data-target': '#${id}-table', 'aria-controls': '${id}-table', 'aria-expanded': 'false', 'html': [
-                    {'<>': "span", 'class': "text-truncate", 'text': "Matches table"},
-                    {'<>': "span", 'class': "card-btn-toggle", 'html': [
-                        {'<>': "span", 'class': "card-btn-toggle-default", 'text': "+"}, 
-                        {'<>': "span", 'class': "card-btn-toggle-active", 'text': "-"}
-                    ]}
-                ]}
-            ]}, 
-            {'<>': "div", 'class': "collapse", 'id': '${id}-table', 'aria-labelledby': '${id}-table-heading', 'data-parent': '#${id}-accordion', 'html': [
-                {'<>': "div", 'class': "card-body text-center", 'html': [
-                    {'[]': "table"}
-                ]}
-            ]}
-        ]},
-    ]
+    if (valid_file && valid_classifier) {
+        valid_form = true;
+        button.disabled = false;
+    } else {
+        valid_form = false;
+        button.disabled = true;
+    }
 }
 
-json2html.component.add('table-accordion',hidden_table_template);
-
-let classifier_template = {
-    '<>': "div", 
-    'class': "border-bottom my-4 py-3",
-    'html': [
-            {'<>': "h2", 'class': 'mb-4', 'text': "${name}"},
-            {"[]": function(){if(this.best_match.good[0]) return("good_match")}},
-            {"[]": function(){if(!this.best_match.good[0]) return("no_match")}},
-            {"[]": "pathogen_result"},
-            {'<>': "div", 'class': "small", 'html': [
-                {'<>': "dl", 'class': "row mb-0", 'html': [
-                    {'<>': "dt", 'class': "col-sm-4 text-secondary", 'text': "Classifier name"},
-                    {'<>': "dd", 'class': "col-sm-6", 'text': "${name}"},
-                ]},
-                {'<>': "dl", 'class': "row mb-0", 'html': [
-                    {'<>': "dt", 'class': "col-sm-4 text-secondary", 'text': "Level"},
-                    {'<>': "dd", 'class': "col-sm-6", 'text': "${type}"},
-                ]},
-                {'<>': "dl", 'class': "row mb-0", 'html': [
-                    {'<>': "dt", 'class': "col-sm-4 text-secondary", 'text': "Kmer database"},
-                    {'<>': "dd", 'class': "col-sm-6", 'text': "${db_path}"},
-                ]},
-            ]},
-            {'[]': "table-accordion"}
-    ]
-}
-
-json2html.component.add('classifier', classifier_template);
-
-let pathogen_result_template = {'<>': "p", 'class': 'mb-4', "html": [
-    {"text": "The pathogen with the most matches is "},
-    {'<>': "span", "class": "font-weight-bold", "text": "${name} ${matches.0.name}"}, 
-    {"text": " with ${matches.0.value} out of ${total_matches} matches "},
-    {"text": function(){ return('(' + Math.round(this.best_match.prop * 100) + '%)')}},
+let results_link_template = {"<>": "div", "class": "alert alert-soft-secondary text-center", "id": "results-alert", "html": [
+    {"<>": "p", "text": "Your pathogen indentification request was successful and your results are ready."},
+    {"<>": "a", "href": "${url}", "text": "View results", "class": "btn btn-secondary"}
 ]}
 
-json2html.component.add('pathogen_result', pathogen_result_template);
-
-let good_match_template = {"<>": "div", "class": "alert alert-soft-primary", "role" : "alert", 
-"html": [
-    {"<>": "p", "class": "font-weight-bold mb-0", "html": "&#x1F60A; We found a good match for your sequence"},
-]}
-
-let bad_match_template = {"<>": "div", "class": "alert alert-soft-danger", "role" : "alert", 
-"html": [
-    {"<>": "p", "class": "font-weight-bold mb-0", "html": "&#x1F641; We did NOT find a good match for your sequence in our database."},
-]}
-
-json2html.component.add('good_match', good_match_template);
-json2html.component.add('no_match', bad_match_template);
-
-let classification_template = {"<>": "div", "html": [
-    {"<>": "h1", "class": "mb-4", "text": "Identification results"},
-    //{"[]": "pathogen_result", "obj" : function(){return(this.classifiers)}},
-    {'<>': "div", 'class': "my-4 py-3 border-bottom small", 'html': [
-        {'<>': "h3", "class": "mb-4", "text": "Request details"},
-        {'<>': "dl", 'class': "row mb-0", 'html': [
-            {'<>': "dt", 'class': "col-sm-4 text-secondary", 'text': "Sequence file"},
-            {'<>': "dd", 'class': "col-sm-6", 'text': "${data_filename}"},
-        ]},
-        {'<>': "dl", 'class': "row mb-0", 'html': [
-            {'<>': "dt", 'class': "col-sm-4 text-secondary", 'text': "Request ID"},
-            {'<>': "dd", 'class': "col-sm-6", 'text': "${request_id}"},
-        ]},
-        {'<>': "dl", 'class': "row mb-0", 'html': [
-            {'<>': "dt", 'class': "col-sm-4 text-secondary", 'text': "Request time"},
-            {'<>': "dd", 'class': "col-sm-6", 'text': "${request_time} UTC"},
-        ]},
-        {'<>': "dl", 'class': "row mb-0", 'html': [
-            {'<>': "dt", 'class': "col-sm-4 text-secondary", 'text': "Processing time"},
-            {'<>': "dd", 'class': "col-sm-6", 'text': "${running_time} s"},
-        ]},
+let error_template = {"<>": "div", "class": "alert alert-soft-danger", "id": "results-alert", "html": [
+    {"<>": "h4", "class":"alert-heading", "html": [
+        {"<>": "i", "class": "fas fa-exclamation-triangle mt-1 mr-1"}, 
+        {"<>": "span", "text": " Sorry"}
     ]},
-    {"[]": "classifier", "obj": function(){return(this.classifiers)}},
-
+    {"<>": "p", "text": "Something went wrong and your pathogen indentification request was unsuccessful. Please contact us and we'll do our best to solve the problem."},
 ]}
+
 
 function fetchpost() {
     // Activate spinner
@@ -138,332 +74,50 @@ function fetchpost() {
 
     // (A) GET FORM DATA
     let data = new FormData();
-    let fileField = document.querySelector('input[type="file"]');
-    data.append("fastq", fileField.files[0]);
+    data.append("fastq", fileField.files[0]); // Add the file
+
+    let classifier = []
+    let checked_checboxes = document.querySelectorAll('input[name="classifier"]:checked');
+    for (let i = 0; i < checked_checboxes.length; i++) {
+        classifier.push(checked_checboxes[i].value)
+    }
+    data.append("classifier", classifier)
+
+    data.append("email", email.value)
+
 
     // (B) FETCH
     fetch("https://rappseq-api-zrvf6kau3q-de.a.run.app/identify", {
-    //fetch("http://127.0.0.1:4869/identify", {
-        
+    //fetch("http://127.0.0.1:1234/identify", {
             method: 'post',
             body: data
         })
         .then(function(response) {
             return response.json();
         })
+        .catch(function(error) {
+            console.log(error)
+        })
         .then(function(text) {
             console.log(text);
             spinner.style.display = "none"
             button.lastElementChild.innerText = "Identify pathogen"
             button.disabled = false
-            //results.append();
-            results.innerHTML = json2html.render(text, classification_template);
+            if (typeof text.request_id !== 'undefined') {
+                let results_url = "../result/?request_id=" + text.request_id;
+                window.location.href = results_url;
+                results.innerHTML = json2html.render({url : results_url}, results_link_template);
+            } else {
+                results.innerHTML = json2html.render({}, error_template);
+            }
+            
+            var resultsAlert = document.getElementById("results-alert");
+            resultsAlert.scrollIntoView();
             //results.innerHTML = json2html.render(text.classifiers[0],table_template);
-        })
-        .catch(function(error) {
-            console.log(error)
         });
 
     // (C) PREVENT HTML FORM SUBMIT
     return false;
 }
-
-// create test data for json2html
-let dummy_data = {
-    "data_filename": [
-      "Mystery4_nano_porechop_400.fastq"
-    ],
-    "data_content_type": [
-      "application/octet-stream"
-    ],
-    "request_id": [
-      "bdb2cf42541aafc2ce493f110bba7313"
-    ],
-    "request_time": [
-      "2021-10-29 02:12:14"
-    ],
-    "running_time": [
-      16.173
-    ],
-    "data_hash": [
-      "c21245232a735279eff56ec9b4b7c78f"
-    ],
-    "classifiers": [
-      {
-        "name": [
-          "Group B Streptococcus- multilocus sequence type"
-        ],
-        "binomial_name": [
-          "Streptococcus agalactiae"
-        ],
-        "type": [
-          "Sequence type"
-        ],
-        "db_path": [
-          "data/classifiers/GBS/210524.db"
-        ],
-        "id": [
-          "sa_mlst"
-        ],
-        "total_matches": [
-          2310
-        ],
-        "best_match": {
-          "good": [
-            false
-          ],
-          "prop": [
-            0.8143
-          ]
-        },
-        "matches": [
-          {
-            "name": [
-              "260"
-            ],
-            "value": [
-              1881
-            ]
-          },
-          {
-            "name": [
-              "103"
-            ],
-            "value": [
-              87
-            ]
-          },
-          {
-            "name": [
-              "22"
-            ],
-            "value": [
-              78
-            ]
-          },
-          {
-            "name": [
-              "261"
-            ],
-            "value": [
-              56
-            ]
-          },
-          {
-            "name": [
-              "617"
-            ],
-            "value": [
-              54
-            ]
-          },
-          {
-            "name": [
-              "23"
-            ],
-            "value": [
-              42
-            ]
-          },
-          {
-            "name": [
-              "26"
-            ],
-            "value": [
-              25
-            ]
-          },
-          {
-            "name": [
-              "609"
-            ],
-            "value": [
-              19
-            ]
-          },
-          {
-            "name": [
-              "17"
-            ],
-            "value": [
-              16
-            ]
-          },
-          {
-            "name": [
-              "552"
-            ],
-            "value": [
-              14
-            ]
-          },
-          {
-            "name": [
-              "61"
-            ],
-            "value": [
-              11
-            ]
-          },
-          {
-            "name": [
-              "1"
-            ],
-            "value": [
-              10
-            ]
-          },
-          {
-            "name": [
-              "7"
-            ],
-            "value": [
-              10
-            ]
-          },
-          {
-            "name": [
-              "12"
-            ],
-            "value": [
-              6
-            ]
-          },
-          {
-            "name": [
-              "459"
-            ],
-            "value": [
-              1
-            ]
-          }
-        ]
-      },
-      {
-        "name": [
-          "Group B Streptococcus- serotype"
-        ],
-        "binomial_name": [
-          "Streptococcus agalactiae"
-        ],
-        "type": [
-          "Serotype"
-        ],
-        "db_path": [
-          "data/classifiers/GBS/210524_sero.db"
-        ],
-        "id": [
-          "sa_sero"
-        ],
-        "total_matches": [
-          21064
-        ],
-        "best_match": {
-          "good": [
-            true
-          ],
-          "prop": [
-            0.984
-          ]
-        },
-        "matches": [
-          {
-            "name": [
-              "Ib"
-            ],
-            "value": [
-              20728
-            ]
-          },
-          {
-            "name": [
-              "Ia"
-            ],
-            "value": [
-              113
-            ]
-          },
-          {
-            "name": [
-              "II"
-            ],
-            "value": [
-              89
-            ]
-          },
-          {
-            "name": [
-              "VI"
-            ],
-            "value": [
-              56
-            ]
-          },
-          {
-            "name": [
-              "V"
-            ],
-            "value": [
-              52
-            ]
-          },
-          {
-            "name": [
-              "III"
-            ],
-            "value": [
-              25
-            ]
-          },
-          {
-            "name": [
-              "IV"
-            ],
-            "value": [
-              1
-            ]
-          }
-        ]
-      },
-      {
-        "name": [
-          "Yersinia ruckeri- serotype"
-        ],
-        "binomial_name": [
-          "Yersinia ruckeri"
-        ],
-        "type": [
-          "Serotype"
-        ],
-        "db_path": [
-          "data/classifiers/GBS/210607.db"
-        ],
-        "id": [
-          "yr_sero"
-        ],
-        "total_matches": [
-          2
-        ],
-        "best_match": {
-          "good": [
-            false
-          ],
-          "prop": [
-            1
-          ]
-        },
-        "matches": [
-          {
-            "name": [
-              "O1a"
-            ],
-            "value": [
-              2
-            ]
-          }
-        ]
-      }
-    ]
-  }
 
   //results.innerHTML = json2html.render(dummy_data, classification_template);
